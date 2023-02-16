08e4b1fb1e658d1991452a9ad9dbdebf4ce7d5c8
AV <92280537+adivaste@users.noreply.github.com>
2023-02-16 11:45:28 +0000

    Model part is almost ready

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from main.models import Account, Image
+
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Image)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Image(models.Model):
+      title = models.CharField(max_length=255, null=True)
+      image = models.ImageField(upload_to="images/", null=False)
+      description = models.TextField(blank=True, null=True)
+      account = models.ForeignKey("Account", on_delete=models.CASCADE)
+
+      def __str__(self):
+            return str(self.title)
+
+class Account(models.Model):
+      user = models.OneToOneField(User, on_delete=models.CASCADE)
+      gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')])
+      avatar = models.ImageField(upload_to="avatar/")
+      
+      def __str__(self):
+            return str(self.user.username)
